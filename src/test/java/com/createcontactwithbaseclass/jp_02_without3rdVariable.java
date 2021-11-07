f0662f0532573a30931453fc006d8baf912a62a2
gvinaykumar9642 <vinaykumaryadav9642@gmail.com>
2021-11-07 12:13:20 +0530

    Branch 3 Commit

---
+++ src/test/java/com/createcontactwithbaseclass/jp_02_without3rdVariable.java
@@ -0,0 +1,14 @@
+package com.createcontactwithbaseclass;
+
+public class jp_02_without3rdVariable {
+
+	public static void main(String[] args) {
+		String s="Desktop";
+		for(int i=s.length()-1;i>=0;i--)
+		{
+			System.out.print(s.charAt(i));
+		}
+
+	}
+
+}
